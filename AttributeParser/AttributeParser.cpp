12478d8a502038846ca0b71b8b4af476589587d7
Todd Erickson <twericks@usc.edu>
2020-01-19 09:53:51 -0800

    updated stuff

--- AttributeParser/AttributeParser.cpp
+++ AttributeParser/AttributeParser.cpp
@@ -12,15 +12,33 @@
 #include <sstream>
 #include <cassert>
 
+struct Attribute {
+   std::string m_Name;
+   std::string m_Value;
+   Attribute()
+      : m_Name(""), m_Value("") {}
+   Attribute(std::string name)
+      : m_Name(name), m_Value("") {}
+   Attribute(std::string name, std::string value)
+      : m_Name(name), m_Value(value) {}
+};
+
 class Tag {
    private:
-   std::list<std::string> m_AttributeList;
+   std::list<Attribute> m_AttributeList;
    std::string m_TagName;
    std::shared_ptr<Tag> m_Parent;
+   /* Tag(const Tag& other) {}
+   Tag& operator=(const Tag& other) {return *this;} */
+
+   public:
+   Tag() : m_AttributeList(), m_TagName(""), m_Parent(nullptr) { }
+   ~Tag() {}
+
    Tag(const Tag& other)
-   : m_AttributeList(other.getAttributeList()),
-   m_TagName(other.getTagName()),
-   m_Parent(other.getParent()) { }
+      : m_AttributeList(other.getAttributeList()),
+      m_TagName(other.getTagName()),
+      m_Parent(other.getParent()) { }
 
    Tag& operator=(const Tag& other) {
       if (this != &other) {
@@ -30,26 +48,58 @@ class Tag {
          return *this;
       }
    }
-   /* Tag(const Tag& other) {}
-   Tag& operator=(const Tag& other) {return *this;} */
 
-   public:
-   Tag() : m_AttributeList(), m_TagName(""), m_Parent(nullptr) { }
-   ~Tag() {}
-
-   std::list<std::string> getAttributeList() const {return m_AttributeList;}
+   std::list<Attribute> getAttributeList() const {return m_AttributeList;}
    std::string getTagName() const {return m_TagName;}
    std::shared_ptr<Tag> getParent() const {return m_Parent;}
+   std::list<Attribute>::iterator findAttribute(Attribute& attribute) const {
+      bool hasSameAttributeName(Attribute pred_attribute) {
+         if (pred_attribute.m_Name == attribute.m_Name) {
+            return true;
+         } else {
+            return false;
+         }
+      }
+      return std::find_if(m_AttributeList.cbegin, m_AttributeList.cend, attribute);
+   }
 
-   void setAttribute(const std::list<std::string>& attributeList) {m_AttributeList=attributeList;}
+   void setAttributeList(const std::list<Attribute>& attributeList) {m_AttributeList=attributeList;}
+   void pushAttribute(const Attribute attribute) {m_AttributeList.push_back(attribute);}
    void setTagName(const std::string& tagName) {m_TagName=tagName;}
    void setParent(std::shared_ptr<Tag> parent) {m_Parent=parent;}
 };
 
+void getAttributes(std::istringstream& iss, Tag& thisTag) {
+   std::string thisWord;
+   std::string attributeName;
+   std::string attributeValue;
+
+   iss >> thisWord;
+   const int END_FLAG = 1;
+   const int NOT_END_FLAG = 0;
+   int endFlag = NOT_END_FLAG;
+   
+   while (">" != thisWord && END_FLAG != endFlag) {
+      attributeName = thisWord;
+
+      iss >> thisWord;
+      assert("=" == thisWord);
+
+      iss >> thisWord;
+      if ('>' == thisWord.back()) {
+         endFlag = 1;
+         thisWord.pop_back();
+      }
+      attributeValue = thisWord;
+
+      Attribute attribute(attributeName, attributeValue);
+      thisTag.pushAttribute(attribute);
+   }
+}
+
 void processLine(std::istream& localCin, std::list<std::shared_ptr<Tag>>& parentList) {
    std::string thisLine;
    char thisChar;
-   std::string thisWord;
 
    std::getline(std::cin, thisLine);
    localCin.get(thisChar);
@@ -64,21 +114,7 @@ void processLine(std::istream& localCin, std::list<std::shared_ptr<Tag>>& parent
    std::string tag;
    iss >> tag;
    thisTag.setTagName(tag);
-
-   iss >> thisWord;
-   if (thisWord == ">") {
-      /* code */
-   }
-   
-
-   std::ostringstream oss;
-
-   
-
-
-   std::string attribute;
-   
-
+   getAttributes(iss, thisTag);
 }
 
 int main() {
